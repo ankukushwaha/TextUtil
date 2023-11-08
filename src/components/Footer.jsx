7f5683b2a6fb42db736047c1a3140270575ee572
ankukushwaha <ankurkushwaha7408@gmail.com>
2023-11-08 21:28:48 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,11 +12,11 @@ function Footer(props) {
 
         <section className='container'>
 
-          <Link className="btn text-white btn-floating m-1" style={{ backgroundColor: "#55acee" }} to="https://twitter.com/home" target="_blank" role="button"><i className="fab fa-twitter"></i></Link>
+          <Link className="btn text-white btn-floating m-1" style={{ backgroundColor: "#55acee" }} to="https://twitter.com/AnkurKumariKus1" target="_blank" role="button"><i className="fab fa-twitter"></i></Link>
 
           {/* <a className="btn text-white btn-floating m-1" style={{ backgroundColor: "#dd4b39" }} to="#!" role="button"><i className="fab fa-google"></i></a> */}
 
-          <Link className="btn text-white btn-floating m-1" style={{ backgroundColor: "#ac2bac" }} to="https://www.instagram.com/" target="_blank" role="button"><i className="fab fa-instagram"></i></Link>
+          <Link className="btn text-white btn-floating m-1" style={{ backgroundColor: "#ac2bac" }} to="https://www.instagram.com/photonista2k20/" target="_blank" role="button"><i className="fab fa-instagram"></i></Link>
 
           <Link className="btn text-white btn-floating m-1" style={{ backgroundColor: "#0082ca" }} to="https://www.linkedin.com/in/ankur-kumari/" target="_blank" role="button"><i className="fab fa-linkedin-in"></i></Link>
 
